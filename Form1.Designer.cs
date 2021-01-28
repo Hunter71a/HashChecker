b66883413cdba0a7427271571ec3884bbc25d0ff
Carl Olson <colson9@student.cscc.edu>
2021-01-28 18:35:02 -0500

    convert all text boxes to rich text area for more flexible formatting

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,23 +31,22 @@ namespace HashChecker
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.processFileButton = new System.Windows.Forms.Button();
-            this.fileHashGood = new System.Windows.Forms.TextBox();
             this.fileHashGoodLabel = new System.Windows.Forms.Label();
             this.title = new System.Windows.Forms.Label();
             this.deluxe = new System.Windows.Forms.Label();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.fileSelectorLabel = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.outputLabel = new System.Windows.Forms.Label();
             this.outputText = new System.Windows.Forms.RichTextBox();
             this.selectFile = new System.Windows.Forms.Button();
-            this.fileSelected = new System.Windows.Forms.TextBox();
+            this.fileSelected = new System.Windows.Forms.RichTextBox();
+            this.fileHashGood = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // processFileButton
             // 
             this.processFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.processFileButton.Location = new System.Drawing.Point(186, 675);
+            this.processFileButton.Location = new System.Drawing.Point(182, 762);
             this.processFileButton.Name = "processFileButton";
             this.processFileButton.Size = new System.Drawing.Size(127, 47);
             this.processFileButton.TabIndex = 0;
@@ -55,20 +54,11 @@ namespace HashChecker
             this.processFileButton.UseVisualStyleBackColor = true;
             this.processFileButton.Click += new System.EventHandler(this.processFileButton_Click);
             // 
-            // fileHashGood
-            // 
-            this.fileHashGood.AccessibleDescription = "fileHashGood";
-            this.fileHashGood.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fileHashGood.Location = new System.Drawing.Point(269, 205);
-            this.fileHashGood.Name = "fileHashGood";
-            this.fileHashGood.Size = new System.Drawing.Size(464, 32);
-            this.fileHashGood.TabIndex = 1;
-            // 
             // fileHashGoodLabel
             // 
             this.fileHashGoodLabel.AutoSize = true;
             this.fileHashGoodLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fileHashGoodLabel.Location = new System.Drawing.Point(73, 211);
+            this.fileHashGoodLabel.Location = new System.Drawing.Point(316, 280);
             this.fileHashGoodLabel.Name = "fileHashGoodLabel";
             this.fileHashGoodLabel.Size = new System.Drawing.Size(170, 26);
             this.fileHashGoodLabel.TabIndex = 2;
@@ -98,20 +88,10 @@ namespace HashChecker
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // fileSelectorLabel
-            // 
-            this.fileSelectorLabel.AutoSize = true;
-            this.fileSelectorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fileSelectorLabel.Location = new System.Drawing.Point(73, 119);
-            this.fileSelectorLabel.Name = "fileSelectorLabel";
-            this.fileSelectorLabel.Size = new System.Drawing.Size(106, 25);
-            this.fileSelectorLabel.TabIndex = 5;
-            this.fileSelectorLabel.Text = "Select file";
-            // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(496, 675);
+            this.button1.Location = new System.Drawing.Point(512, 762);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(153, 47);
             this.button1.TabIndex = 6;
@@ -123,7 +103,7 @@ namespace HashChecker
             // 
             this.outputLabel.AutoSize = true;
             this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputLabel.Location = new System.Drawing.Point(301, 297);
+            this.outputLabel.Location = new System.Drawing.Point(316, 407);
             this.outputLabel.Name = "outputLabel";
             this.outputLabel.Size = new System.Drawing.Size(177, 25);
             this.outputLabel.TabIndex = 7;
@@ -132,7 +112,7 @@ namespace HashChecker
             // outputText
             // 
             this.outputText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputText.Location = new System.Drawing.Point(78, 345);
+            this.outputText.Location = new System.Drawing.Point(78, 435);
             this.outputText.Name = "outputText";
             this.outputText.Size = new System.Drawing.Size(678, 305);
             this.outputText.TabIndex = 8;
@@ -141,37 +121,46 @@ namespace HashChecker
             // selectFile
             // 
             this.selectFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.selectFile.Location = new System.Drawing.Point(622, 119);
+            this.selectFile.Location = new System.Drawing.Point(321, 128);
             this.selectFile.Name = "selectFile";
-            this.selectFile.Size = new System.Drawing.Size(111, 36);
+            this.selectFile.Size = new System.Drawing.Size(150, 36);
             this.selectFile.TabIndex = 9;
-            this.selectFile.Text = "Open";
+            this.selectFile.Text = "Select File";
             this.selectFile.UseVisualStyleBackColor = true;
             this.selectFile.Click += new System.EventHandler(this.selectFile_Click);
             // 
             // fileSelected
             // 
-            this.fileSelected.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fileSelected.Location = new System.Drawing.Point(207, 119);
+            this.fileSelected.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.fileSelected.Location = new System.Drawing.Point(78, 183);
             this.fileSelected.Name = "fileSelected";
-            this.fileSelected.Size = new System.Drawing.Size(396, 31);
-            this.fileSelected.TabIndex = 10;
+            this.fileSelected.Size = new System.Drawing.Size(678, 56);
+            this.fileSelected.TabIndex = 11;
+            this.fileSelected.Text = "";
+            // 
+            // fileHashGood
+            // 
+            this.fileHashGood.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileHashGood.Location = new System.Drawing.Point(78, 318);
+            this.fileHashGood.Name = "fileHashGood";
+            this.fileHashGood.Size = new System.Drawing.Size(678, 59);
+            this.fileHashGood.TabIndex = 12;
+            this.fileHashGood.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(840, 787);
+            this.ClientSize = new System.Drawing.Size(840, 837);
+            this.Controls.Add(this.fileHashGood);
             this.Controls.Add(this.fileSelected);
             this.Controls.Add(this.selectFile);
             this.Controls.Add(this.outputText);
             this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.fileSelectorLabel);
             this.Controls.Add(this.deluxe);
             this.Controls.Add(this.title);
             this.Controls.Add(this.fileHashGoodLabel);
-            this.Controls.Add(this.fileHashGood);
             this.Controls.Add(this.processFileButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
@@ -185,17 +174,16 @@ namespace HashChecker
         #endregion
 
         private System.Windows.Forms.Button processFileButton;
-        private System.Windows.Forms.TextBox fileHashGood;
         private System.Windows.Forms.Label fileHashGoodLabel;
         private System.Windows.Forms.Label title;
         private System.Windows.Forms.Label deluxe;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Label fileSelectorLabel;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label outputLabel;
         private System.Windows.Forms.RichTextBox outputText;
         private System.Windows.Forms.Button selectFile;
-        private System.Windows.Forms.TextBox fileSelected;
+        private System.Windows.Forms.RichTextBox fileSelected;
+        private System.Windows.Forms.RichTextBox fileHashGood;
     }
 }
 
