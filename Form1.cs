ec4ddeb5b90a12e6ca1b4b406ad39eb58f802ae7
Carl Olson <colson9@student.cscc.edu>
2021-01-30 16:35:13 -0500

    update textboxes to rich text area for greater flexability of formatting

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+
 
 namespace HashChecker
 {
@@ -21,6 +25,60 @@ namespace HashChecker
         private string hashCodeGood = "";
         private string output = "After selecting your file and entering the hash key, relax while the Hash-O-Matic XL3000 does all the work";
         private string testFileLocation = "";
+        private string sha256computed = "";
+        private string directory = "";
+        private string fileName = "";
+        private string generatedKey = "";
+        private Byte[] hashValue;
+       // private FileStream file;
+        private FileInfo src = null;
+
+
+        /* test file 
+         
+         Name: a.7z
+        Size: 1201092 bytes (1172 KiB)
+        SHA256: 55BD7777EDD36CB10CA2F735DC98DAD2AA70CCDDEE072D4226BE45F108D0726A
+         *
+         */
+
+
+
+        // The cryptographic service providers.
+        private SHA256 Sha256 = SHA256.Create();
+
+
+
+
+
+        // Utilities
+
+        // Return a byte array as a sequence of hex values.
+        public static string BytesToString(byte[] bytes)
+        {
+            string result = "";
+            foreach (byte b in bytes) result += b.ToString("x2");
+            return result;
+        }
+
+        // event handler for exitButton to exit program safely
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Display the byte array in a readable format.
+        public static void PrintByteArray(byte[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]:X2}");
+                if ((i % 4) == 3) Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+
+
 
         /*
          * event handler for selectFile button
@@ -31,15 +89,17 @@ namespace HashChecker
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog
             {
-                InitialDirectory = @"D:\",
-                Title = "Browse Text Files",
+                InitialDirectory = @"C:\",
+                Title = "Browse for file to check",
 
                 CheckFileExists = true,
                 CheckPathExists = true,
 
-                DefaultExt = "txt",
-                Filter = "txt files (*.txt)|*.txt",
-                FilterIndex = 2,
+
+
+                //  DefaultExt = "txt",
+                //    Filter = "txt files (*.txt)|*.txt",
+                //   FilterIndex = 2,
                 RestoreDirectory = true,
 
                 ReadOnlyChecked = true,
@@ -48,14 +108,18 @@ namespace HashChecker
 
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                fileSelected.Text = openFileDialog1.FileName;
-                testFileLocation = fileSelected.Text;
+                testFileLocation = openFileDialog1.FileName;
+                var file = openFileDialog1.OpenFile();
+                fileName = openFileDialog1.SafeFileName;
+                src = new FileInfo(testFileLocation);
+                fileSelected.Text = src.Name;
+                outputText.Text = $"Entered hash key is {fileHashGood} \n" +
+$"and the file selected is {testFileLocation}\n {src.Name}\n src length = {src.Length} \n after hexadecimal conversion to utf-8: \n {src.LastAccessTime}";
             }
-
         }
 
 
-        
+
         /*  
          *  Check hash from fileHashGood and compare to powershell generated results
          *  
@@ -64,32 +128,92 @@ namespace HashChecker
          */
         private void processFileButton_Click(object sender, EventArgs e)
         {
+           
             if (fileHashGood.Text == "")
             {
                 MessageBox.Show("You need to include the hash key to check your file", "Incorrect Information",
                                    MessageBoxButtons.OK, MessageBoxIcon.Stop);
                 fileHashGood.Focus();
             }
-            outputText.Text = $"Entered hash key is {fileHashGood.Text} \n" +
-                $"and the file selected is {testFileLocation}\n" +
-                $"Dude, you're totally screwed";
+
+
+
+            // Initialize a SHA256 hash object.
+            else if (src != null && fileHashGood.Text != "")
+            {
+                using (Sha256)
+                {
+                    try
+                    {
+                        // Create a fileStream for the file.
+                        FileStream fileStream = src.Open(FileMode.Open);
+                        // Be sure it's positioned to the beginning of the stream.
+                        fileStream.Position = 0;
+                        // Compute the hash of the fileStream.
+                        hashValue = Sha256.ComputeHash(fileStream);
+                        generatedKey = BytesToString(hashValue);
+                        // Write the name and hash value of the file to the console.
+                        outputText.Text = $"Entered hash key is {fileHashGood} \n" +
+    $"and the file selected is {testFileLocation}\n {src.Name}\n raw hash = {hashValue} \n after hexadecimal conversion to utf-8: \n {BytesToString(hashValue)}";
+                        Console.Write($"{src.Name}: ");
+                        PrintByteArray(hashValue);
+                        // Close the file.
+                        fileStream.Close();
+                    }
+                    catch (IOException E)
+                    {
+                        Console.WriteLine($"I/O Exception: {E.Message}");
+                    }
+                    catch (UnauthorizedAccessException E)
+                    {
+                        Console.WriteLine($"Access Exception: {E.Message}");
+                    }
+                }
+            }
+
         }
+          
+    
 
 
 
+        // Open file stream to read data for hash check
+     //   StreamReader fileStream = new StreamReader(testFileLocation);
+      //      var fileContent = fileStream.ReadToEnd();
 
-        // event handler for exitButton to exit program safely
-        private void exitButton_Click(object sender, EventArgs e)
+
+            // check sha256 to test .Cryptography approach
+             //   byte hashedData = Sha256.ComputeHash(fileContent.ToString());  
+
+         //   sha256computed = ComputeSha256Hash()
+            
+
+
+
+        
+
+        // method for computing sha256 hash
+        private string ComputeSha256Hash(string rawData)
         {
-            this.Close();
-        }
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
 
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             outputText.Text = output;
-        }
-
-       
+        }       
     }
 }
